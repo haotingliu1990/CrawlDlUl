c4110020e0d78d530ef54cc7071317455787fd65
haotingliu1990 <haotingliu1990@gmail.com>
2018-04-12 14:15:54 -0700

    Create Project.

---
+++ src/main/java/com/haotliu/imageoperation/ImageoperationApplication.java
@@ -0,0 +1,12 @@
+package com.haotliu.imageoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageoperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageoperationApplication.class, args);
+    }
+}
